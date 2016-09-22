8f06c26f94be7b22f49e295aa15ad52bc008d9f0
iamscottcox <scottcox89@gmail.com>
2016-09-22 23:02:51 +0100

    Assets => Styles -> Started .feature-header

--- web/app/themes/sage/front-page.php
+++ web/app/themes/sage/front-page.php
@@ -1,7 +1,8 @@
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-xs-12 text-center">
+<div class="container-fluid feature-header">
+  <div class="row flex-items-center">
+    <div class="col-xs flex-xs-middle">
       <h1>Scott Cox</h1>
+      <h2>Web Developer & Designer</h2>
     </div>
   </div>
 </div>
