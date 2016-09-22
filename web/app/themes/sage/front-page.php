35392b695c72153b95dce8f11e6ed15b666d9335
iamscottcox <scottcox89@gmail.com>
2016-09-22 23:36:39 +0100

    Assets => Styles => .feature-header -> Chevron placeholder

--- web/app/themes/sage/front-page.php
+++ web/app/themes/sage/front-page.php
@@ -1,8 +1,9 @@
 <div class="container-fluid feature-header">
-  <div class="row flex-items-center">
-    <div class="col-xs flex-xs-middle">
-      <h1>Scott Cox</h1>
+  <div class="row flex-items-center feature-header-row-one">
+    <div class="col-xs flex-xs-middle feature-header-headings">
+      <h1><span>Scott Cox</span></h1>
       <h2>Web Developer & Designer</h2>
     </div>
+    <p>CHEVRON</p>
   </div>
 </div>
