7241561b374d62accb6029a845f4f3d5b5618d45
iamscottcox <scottcox89@gmail.com>
2016-09-26 20:04:18 +0100

    Front Page => .feature-header -> Added background-clip and image to header

--- web/app/themes/sage/front-page.php
+++ web/app/themes/sage/front-page.php
@@ -2,7 +2,7 @@
   <div class="row flex-items-center feature-header-row">
     <div class="col-xs flex-xs-middle feature-header-headings">
       <h1><span>Scott Cox</span></h1>
-      <h2>Web Developer & Designer</h2>
+      <h2><span>Web Developer & Designer</span></h2>
     </div>
     <a href="#"><i class="fa fa-chevron-down"></i></a>
   </div>
