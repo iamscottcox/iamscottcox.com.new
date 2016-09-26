accd4cec8dc9a06996585de16e0bde742c5923ed
iamscottcox <scottcox89@gmail.com>
2016-09-26 23:04:10 +0100

    Global => Navigation -> Changed main menu PHP

--- web/app/themes/sage/lib/setup.php
+++ web/app/themes/sage/lib/setup.php
@@ -27,7 +27,8 @@ function setup() {
   // Register wp_nav_menu() menus
   // http://codex.wordpress.org/Function_Reference/register_nav_menus
   register_nav_menus([
-    'primary_navigation' => __('Primary Navigation', 'sage')
+    'navbar-left-side' => __('Navbar Left Side', 'sage'),
+    'navbar-right-side' => __('Navbar Right Side', 'sage')
   ]);
 
   // Enable post thumbnails
