759fa5c894da798e38024683acebf27c9bb5e932
iamscottcox <scottcox89@gmail.com>
2016-09-26 19:27:56 +0100

    Assets => Styles => .feature-header -> Added round button chevron

--- web/app/themes/sage/front-page.php
+++ web/app/themes/sage/front-page.php
@@ -1,9 +1,9 @@
 <div class="container-fluid feature-header">
-  <div class="row flex-items-center feature-header-row-one">
+  <div class="row flex-items-center feature-header-row">
     <div class="col-xs flex-xs-middle feature-header-headings">
       <h1><span>Scott Cox</span></h1>
       <h2>Web Developer & Designer</h2>
     </div>
-    <p>CHEVRON</p>
+    <a href="#">V</a>
   </div>
 </div>
