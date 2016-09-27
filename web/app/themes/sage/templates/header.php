6d330f0403a809408e8f8d1b1f962873d5d8e9de
iamscottcox <scottcox89@gmail.com>
2016-09-27 23:35:51 +0100

    Header => Links -> Added border underline on hover

--- web/app/themes/sage/templates/header.php
+++ web/app/themes/sage/templates/header.php
@@ -4,10 +4,6 @@
       <?php if (has_nav_menu('navbar-left-side')):
         wp_nav_menu(['theme_location' => 'navbar-left-side', 'menu_class' => 'nav']);
       endif; ?>
-      <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-      <?php if (has_nav_menu('navbar-right-side')):
-      wp_nav_menu(['theme_location' => 'navbar-right-side', 'menu_class' => 'nav']);
-      endif; ?>
     </nav>
   </div>
 </header>
