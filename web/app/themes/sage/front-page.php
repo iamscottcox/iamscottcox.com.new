1bb15140020e8fc9412f16c7afa46f95e8194329
iamscottcox <scottcox89@gmail.com>
2016-10-01 13:58:01 +0100

    Header => Background -> Matches HTML, but still overlays content

--- web/app/themes/sage/front-page.php
+++ web/app/themes/sage/front-page.php
@@ -1,4 +1,4 @@
-<div class="container-fluid feature-header">
+<section class="container-fluid feature-header">
   <div class="row flex-items-center feature-header-row">
     <div class="col-xs flex-xs-middle feature-header-headings">
       <h1><span>Scott Cox</span></h1>
@@ -6,4 +6,13 @@
     </div>
     <a href="#"><i class="fa fa-chevron-down"></i></a>
   </div>
-</div>
+</section>
+<section class="full-height-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+
+      </div>
+    </div>
+  </div>
+</section>
