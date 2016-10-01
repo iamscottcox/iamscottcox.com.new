bb5cb46c3d8ff403e9fe821b5ea3c30da5b02e7c
iamscottcox <scottcox89@gmail.com>
2016-10-01 16:45:09 +0100

    .feature-header => <span> -> Removed due to jank

--- web/app/themes/sage/front-page.php
+++ web/app/themes/sage/front-page.php
@@ -1,8 +1,8 @@
 <section class="container-fluid feature-header">
   <div class="row flex-items-center feature-header-row">
     <div class="col-xs flex-xs-middle feature-header-headings">
-      <h1><span>Scott Cox</span></h1>
-      <h2><span>Web Developer & Designer</span></h2>
+      <h1>Scott Cox</h1>
+      <h2>Web Developer & Designer</h2>
     </div>
     <a href="#"><i class="fa fa-chevron-down"></i></a>
   </div>
