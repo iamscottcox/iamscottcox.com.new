30a13c7b91fa9adbe8fe4bf5e859308405216881
iamscottcox <scottcox89@gmail.com>
2016-10-24 23:13:04 +0100

    Added about-me.section

---
+++ web/app/themes/sage/templates/sections/about-me.section.php
@@ -0,0 +1,12 @@
+<section id="section-about-me">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <h2 class="section-header">
+          About Me
+        </h2>
+      </div>
+    </div>
+  </div>
+  <?php get_template_part('templates/components/about-me.component'); ?>
+</section>
