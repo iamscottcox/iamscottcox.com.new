f94a78ea84b2c77013d651175495c450b61b2f17
iamscottcox <scottcox89@gmail.com>
2016-11-14 22:47:25 +0000

    Changes made to recent work content cards

--- web/app/themes/sage/templates/components/recent-work.component.php
+++ web/app/themes/sage/templates/components/recent-work.component.php
@@ -7,10 +7,12 @@ $context = Timber::get_context();
 $context['posts'] = [
   (new Content_Block())
     ->setTitle('<h2>Hello World!</h2>')
-    ->setContent('<p>Synth squid twee blog actually master cleanse. Normcore skateboard irony, hella vice yuccie fanny pack hexagon brooklyn man bun before they sold out pinterest. Mustache 90\'s everyday carry, mumblecore occupy neutra gentrify YOLO readymade street art actually knausgaard 3 wolf moon. Truffaut affogato humblebrag everyday carry organic pickled. Pickled chambray quinoa gastropub freegan, kogi tbh organic coloring book. Chillwave vice williamsburg enamel pin, banh mi fam biodiesel scenester. Scenester locavore chicharrones, direct trade semiotics ennui humblebrag PBR&B man braid.</p>'),
+    ->setContent('<p>Synth squid twee blog actually master cleanse. Normcore skateboard irony, hella vice yuccie fanny pack hexagon brooklyn man bun before they sold out pinterest. Mustache 90\'s everyday carry, mumblecore occupy neutra gentrify YOLO readymade street art actually knausgaard 3 wolf moon. Truffaut affogato humblebrag everyday carry organic pickled. Pickled chambray quinoa gastropub freegan, kogi tbh organic coloring book. Chillwave vice williamsburg enamel pin, banh mi fam biodiesel scenester. Scenester locavore chicharrones, direct trade semiotics ennui humblebrag PBR&B man braid.</p>')
+  ->setImg('<img src="http://lorempixel.com/500/500" alt="placeholder">'),
   (new Content_Block())
     ->setTitle('<h2>Hello World!</h2>')
     ->setContent('<p>Synth squid twee blog actually master cleanse. Normcore skateboard irony, hella vice yuccie fanny pack hexagon brooklyn man bun before they sold out pinterest. Mustache 90\'s everyday carry, mumblecore occupy neutra gentrify YOLO readymade street art actually knausgaard 3 wolf moon. Truffaut affogato humblebrag everyday carry organic pickled. Pickled chambray quinoa gastropub freegan, kogi tbh organic coloring book. Chillwave vice williamsburg enamel pin, banh mi fam biodiesel scenester. Scenester locavore chicharrones, direct trade semiotics ennui humblebrag PBR&B man braid.</p>')
+    ->setImg('<img src="http://lorempixel.com/500/500" alt="placeholder">')
 ];
 
 Timber::render('recent-work-and-posts.view.twig', $context);
