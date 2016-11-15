f86fd5ee68c07463c831c281a2063046bee8da3f
iamscottcox <scottcox89@gmail.com>
2016-11-15 19:49:47 +0000

    recent-posts.component now uses the WP loop

--- web/app/themes/sage/templates/components/recent-posts.component.php
+++ web/app/themes/sage/templates/components/recent-posts.component.php
@@ -7,19 +7,9 @@
  */
 
 use Timber\Timber;
-use Templates\Classes\Content_Block;
 
 $context = Timber::get_context();
-$context['posts'] = [
-  (new Content_Block())
-    ->setTitle('<h2>Hello World!</h2>')
-    ->setContent('<p>Synth squid twee blog actually master cleanse. Normcore skateboard irony, hella vice yuccie fanny pack hexagon brooklyn man bun before they sold out pinterest. Mustache 90\'s everyday carry, mumblecore occupy neutra gentrify YOLO readymade street art actually knausgaard 3 wolf moon. Truffaut affogato humblebrag everyday carry organic pickled. Pickled chambray quinoa gastropub freegan, kogi tbh organic coloring book. Chillwave vice williamsburg enamel pin, banh mi fam biodiesel scenester. Scenester locavore chicharrones, direct trade semiotics ennui humblebrag PBR&B man braid.</p>')
-    ->setImg('<img src="http://lorempixel.com/500/500" alt="placeholder">'),
-  (new Content_Block())
-    ->setTitle('<h2>Hello World!</h2>')
-    ->setContent('<p>Synth squid twee blog actually master cleanse. Normcore skateboard irony, hella vice yuccie fanny pack hexagon brooklyn man bun before they sold out pinterest. Mustache 90\'s everyday carry, mumblecore occupy neutra gentrify YOLO readymade street art actually knausgaard 3 wolf moon. Truffaut affogato humblebrag everyday carry organic pickled. Pickled chambray quinoa gastropub freegan, kogi tbh organic coloring book. Chillwave vice williamsburg enamel pin, banh mi fam biodiesel scenester. Scenester locavore chicharrones, direct trade semiotics ennui humblebrag PBR&B man braid.</p>')
-    ->setImg('<img src="http://lorempixel.com/500/500" alt="placeholder">')
-];
+$context['posts'] = Timber::get_posts();
 
 Timber::render('recent-work-and-posts.view.twig', $context);
 
