4618ce181deb82196482bdcab0c52ff50bdc4526
iamscottcox <scottcox89@gmail.com>
2016-11-15 19:49:09 +0000

    Added more information to about-me.section

--- web/app/themes/sage/templates/sections/about-me.section.php
+++ web/app/themes/sage/templates/sections/about-me.section.php
@@ -15,7 +15,11 @@
         </div>
         <p>
           I am a <span id="age">27</span> year old Web Developer who has been coding
-          <span id="coding-for">since May 4<sup>th</sup> 2016</span>, and I am based close to the city of Bath in the UK. I am passionate about everything and anything to do with the web, and I love trying out new tools. I'm forward thinking, determined, and I'm always keen to take on new challenges. If I'm not writing code, I'm busy with my band.
+          <span id="coding-for">since May 4<sup>th</sup> 2016</span>, and I am based close to the city of Bath in the UK.</p>
+        <br aria-hidden="true" />
+        <p>I currently work for <a href="https://animalfriends.co.uk">Animal Friends Insurance</a> in Amesbury as a Front End Web Developer.</p>
+        <br aria-hidden="true" />
+        <p>My main focus is working with the front end, but I do have a growing interest in back end development too, especially when it comes to MEAN stacks. I am passionate about everything and anything to do with the web, and I love trying out new tools. I'm forward thinking, determined, and I'm always keen to take on new challenges. If I'm not writing code, I'm busy with my band.
         </p>
       </div>
     </div>
