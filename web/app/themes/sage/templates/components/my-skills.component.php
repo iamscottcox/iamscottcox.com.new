50f2dc0d84212af12d0108c5b0983a9ff4e8415e
Scott Cox <scottcox89@gmail.com>
2016-11-20 18:58:40 +0000

    Skills posts integration

--- web/app/themes/sage/templates/components/my-skills.component.php
+++ web/app/themes/sage/templates/components/my-skills.component.php
@@ -1,41 +1,14 @@
 <?php
 
 use Timber\Timber;
-use Templates\Classes\Content_Block;
 
 $context = Timber::get_context();
+$args = array(
+    'post_type' => 'skills',
+    'posts_per_page' => 10
+);
 
-$context['posts'] = [
-  (new Content_Block())
-    ->setTitle('<h4>HTML</h4>')
-    ->setImg('<img src="http://placehold.it/300x300" alt="placeholder">'),
-  (new Content_Block())
-    ->setTitle('<h4>CSS</h4>')
-    ->setImg('<img src="http://placehold.it/300x300" alt="placeholder">'),
-  (new Content_Block())
-    ->setTitle('<h4>JavaScript</h4>')
-    ->setImg('<img src="http://placehold.it/300x300" alt="placeholder">'),
-  (new Content_Block())
-    ->setTitle('<h4>jQuery</h4>')
-    ->setImg('<img src="http://placehold.it/300x300" alt="placeholder">'),
-  (new Content_Block())
-    ->setTitle('<h4>React</h4>')
-    ->setImg('<img src="http://placehold.it/300x300" alt="placeholder">'),
-  (new Content_Block())
-    ->setTitle('<h4>SCSS</h4>')
-    ->setImg('<img src="http://placehold.it/300x300" alt="placeholder">'),
-  (new Content_Block())
-    ->setTitle('<h4>Git</h4>')
-    ->setImg('<img src="http://placehold.it/300x300" alt="placeholder">'),
-  (new Content_Block())
-    ->setTitle('<h4>Babel</h4>')
-    ->setImg('<img src="http://placehold.it/300x300" alt="placeholder">'),
-  (new Content_Block())
-    ->setTitle('<h4>Webpack</h4>')
-    ->setImg('<img src="http://placehold.it/300x300" alt="placeholder">'),
-  (new Content_Block())
-    ->setTitle('<h4>Terminal</h4>')
-    ->setImg('<img src="http://placehold.it/300x300" alt="placeholder">'),
-];
+$context = Timber::get_context();
+$context['posts'] = Timber::get_posts($args);
 
-Timber::render('my-skills.view.twig', $context);
+Timber::render('posts.view.twig', $context);
