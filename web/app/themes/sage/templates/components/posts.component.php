f9918e9511ff1d4af0134e7dde5df25c4e0e7d1a
Scott Cox <scottcox89@gmail.com>
2016-11-20 14:31:13 +0000

    Added pagination to blog page

--- web/app/themes/sage/templates/components/posts.component.php
+++ web/app/themes/sage/templates/components/posts.component.php
@@ -2,12 +2,21 @@
 
 use Timber\Timber;
 
-$args = [
+global $paged;
+if (!isset($paged) || !$paged){
+    $paged = 1;
+}
+$context = Timber::get_context();
+$args = array(
     'post_type' => 'post',
     'posts_per_page' => 10,
-];
+    'paged' => $paged
+);
+
+query_posts($args);
 
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts($args);
+$context['pagination'] = Timber::get_pagination();
 
 Timber::render('posts.view.twig', $context);
